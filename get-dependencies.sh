4e8522e009213cab960a107b9ad60c6922b6c5fd
Link4Electronics <link4electronics@gmail.com>
2026-03-08 21:51:58 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -23,8 +23,8 @@ get-debloated-pkgs --add-common --prefer-nano
 echo "Getting app..."
 echo "---------------------------------------------------------------"
 case "$ARCH" in # they use X64 and ARM64 for the zip links
-	x86_64)  zip_arch=Linux-X64;;
-	aarch64) zip_arch=Linux-ARM64;;
+	x86_64)  zip_arch=Linux-X64-Release;;
+	aarch64) zip_arch=Linux-ARM64-Release;;
 esac
 ZIP_LINK=$(wget -qO- https://api.github.com/repos/sonicdcer/MarioKart64Recomp/releases \
       | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*MarioKart64Recompiled.*$zip_arch.zip")
